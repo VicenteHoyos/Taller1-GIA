c3ffc16f5bf282b4bab646924838b0faf160a153
NICOLAS <NICOLAS@PC-VICENT>
2020-03-30 22:53:57 -0500

    creacion servicio rest

---
+++ src/main/java/co/edu/utp/isc/gia/hello/HelloApplication.java
@@ -0,0 +1,30 @@
+package co.edu.utp.isc.gia.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+        
+@SpringBootApplication
+@RestController
+@RequestMapping("hello")
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+        @GetMapping("/{firstName}/{lastName}")
+        public String sayHellow(
+        @PathVariable ("firstName") String firstName,
+        @PathVariable ("lastName") String lastName){
+            return String.format("Hello %s %s", firstName, lastName);
+        }
+        @GetMapping()
+        public String sayHello2(@RequestParam("firstName") String firstName,@RequestParam("lastName") String lastName){
+            return String.format("Hello %s %s",firstName,lastName);
+        }
+
+}
